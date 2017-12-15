08a3da25c58eb35e81aca21c2172a49a198511ca
Jens Hoffmann <xmcpam@gmail.com>
2017-12-15 09:35:18 +0100

    Turn off menu bar/scroll bar/tool bar early

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+;; .emacs.d/init.el - the entry point to my emacs config
+
+;; Turn off menu bar/tool bar scroll bar early
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 ;; Install packages
 (require 'package)
 
@@ -38,11 +45,6 @@
       (package-install package))))
 
 (install-packages)
-
-;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; Remove trailing whitespaces
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
